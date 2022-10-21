888bc3843ec7cfdb109a3440f57a8fbc97eb3616
mayank0801 <36763315+mayank0801@users.noreply.github.com>
2022-10-22 05:28:35 +0530

    fix: code logic

--- index.js
+++ index.js
@@ -1,53 +1,46 @@
-const readLineSync = require("readline-sync");
+const readLineSync = require("readline-sync")
 const userName = readLineSync.question("What is your name? ");
 console.log(`Hello ${userName.toUpperCase()}!!!
 Welcome!!
 WHAT DO YOU KNOW ABOUT MAYANK 😊`);
 
 var Score = 0;
-const helper = function (question, answer) {
+const helper = function(question, answer) {
   var userAns = readLineSync.question(question);
 
-  if (userAns === answer) {
+  if (userAns.toLowerCase() === answer.toLowerCase()) {
     console.log("Right!!");
     Score++;
-  } else {
+  }
+  else {
     console.log("Wrong answer!");
   }
-};
 
-var questions = [
-  {
-    Question: "What is my homeTown? ",
-    Answer: "Muzaffarpur",
-  },
-  {
-    Question: "What sports do i play the most? ",
-    Answer: "Cricket",
-  },
-  {
-    Question: "Who is my favourite cricketer? ",
-    Answer: "Sachin",
-  },
-  {
-    Question: "What is my favourite color? ",
-    Answer: "Red",
-  },
-  {
-    Question:
-      "What is my favourite food? (Rajma-chawal/Chole-bhature/Chicken-biryani)",
-    Answer: "Chicken-biryani",
-  },
-];
+}
+
+var questions = [{
+  Question: "What is my homeTown? ",
+  Answer: "muzaffarpur",
+}, {
+  Question: "What sports do i play the most? ",
+  Answer: "cricket",
+}, {
+  Question: "Who is my favourite cricketer? ",
+  Answer: "sachin",
+}, {
+  Question: "What is my favourite color? ",
+  Answer: "red",
+}, {
+  Question: "What is my favourite food? (rajma-chawal/chole-bhature/chicken-biryani)",
+  Answer: "biryani",
+}]
 
 for (var i = 0; i < questions.length; i++) {
   var currentQuestion = questions[i];
   // helper(questions[i].Question, questions[i].Answer);
   helper(currentQuestion.Question, currentQuestion.Answer);
+
 }
 
-if (Score > 0) {
-  console.log(`Great!! Your score is: ${Score}`);
-} else {
-  console.log("Never mind! Your score is", Score);
-}
+if (Score > 0) { console.log(`Great!! Your score is: ${Score}`); }
+else { console.log("Never mind! Your score is", Score) }
